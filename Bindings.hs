a162d14bc3d949c2f8bde6d50f24b40df9fce9cd
CSchrisD <crduran@ucsc.edu>
2016-03-06 21:45:14 -0800

    uncomment ship b to redraw ship after leaving screen but it wont move

--- Bindings.hs
+++ Bindings.hs
@@ -8,15 +8,15 @@ import Types
 reshape :: ReshapeCallback
 reshape size = do 
   viewport $= (Position 0 0, size)
- 
+
 keyboardMouse :: IOGLfPair -> IOGLf -> KeyboardMouseCallback
 keyboardMouse delta shipAngle _key _state _modifiers _position = case _key of
    (Char 'w') -> delta $~! \(x,y) -> (x,y+0.001)
-   --(Char 's') -> delta $~! \(x,y) -> (x,y-0.001)
+   (Char 's') -> delta $~! \(x,y) -> (x,y-0.001)
    (Char 'a') -> shipAngle $~! \x -> (x + 0.001)
    (Char 'd') -> shipAngle $~! \x -> x - 0.001
    (SpecialKey KeyUp   ) -> delta $~! \(x,y) -> (x,y+0.001)
-   --(SpecialKey KeyDown ) -> delta $~! \(x,y) -> (x,y-0.001)
+   (SpecialKey KeyDown ) -> delta $~! \(x,y) -> (x,y-0.001)
    (SpecialKey KeyLeft ) -> shipAngle $~! \x -> (x + 0.001)
    (SpecialKey KeyRight) -> shipAngle $~! \x -> x - 0.001
    _ -> return ()
--- Display.hs
+++ Display.hs
@@ -6,15 +6,27 @@ import Data.IORef
 import Designs
 import Types
  
+outFrame:: (GLfloat, GLfloat) -> (GLfloat, GLfloat)
+outFrame (x, y) 
+             | y < 0.9 && y > -0.9 = (x,y)
+             | y > 0.9 = (0, 0)
+             | y < -0.9 = (0, 0)
+             | otherwise = (x, y)
+                  
+
 display ::IOGLfPair -> IOGLf -> DisplayCallback
 display pos shipAngle = do 
   clear [ColorBuffer]
   loadIdentity
   a <- get pos
+--  print a
+  let b = outFrame a
+--  print b
   angle <- get shipAngle
   rotate angle $ Vector3 0 0 1
   preservingMatrix $ do
-    ship a
-    --rock 2 rockTwo
+     ship a
+  --   ship b
+  --   rock 2 rockTwo
   flush
   swapBuffers -- Refreshes screen, necessary when double buffered.
